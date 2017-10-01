cdbb9aa8e7ec8241592b5bdfdcee4816be139a65
Dolychuck <Dolychuck@hotmail.com>
2017-10-01 00:44:17 -0700

    Add movement

--- SamuraiKanjiPirate/Assets/Scripts/PlayerManager.cs
+++ SamuraiKanjiPirate/Assets/Scripts/PlayerManager.cs
@@ -3,22 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
+	public float speedX;
+	public float jumpSpeedY;
+
+	bool facingRight;
+	float speed;
+
 	Animator anim;
+	Rigidbody2D rb;
+
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-
+		rb = GetComponent<Rigidbody2D> ();
+		facingRight = true;
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.W)) {
+		MovePlayer (speed);
+		Flip ();
+
+		//Move left
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			speed = -speedX;
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			speed = 0;
+		}
+
+		//Move right
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			speed = speedX;
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			speed = 0;
+		}
+
+		//Move up
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			anim.SetBool("Jump",true);
+			rb.AddForce (new Vector2 (rb.velocity.x,jumpSpeedY));
+			anim.SetInteger ("State", 2);
+		}
+	}
+
+	void Flip() {
+		if(speed > 0 && !facingRight || speed < 0 && facingRight) {
+			facingRight = !facingRight;
+			Vector3 temp = transform.localScale;
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+	}
+
+	void MovePlayer(float playerSpeed) {
+		if (playerSpeed < 0 && anim.GetBool ("Jump") == false || playerSpeed > 0 &&  anim.GetBool ("Jump") == false) {
 			anim.SetInteger ("State", 1);
-		} 
-		if (Input.GetKeyUp (KeyCode.W)) {
+		}
+		if (playerSpeed == 0 && anim.GetBool ("Jump") == false) {
 			anim.SetInteger ("State", 0);
-		} 
+		}
+		rb.velocity = new Vector3 (speed, rb.velocity.y, 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Ground") {
+			anim.SetBool("Jump",false);
+			anim.SetInteger ("State", 0);
+		}
 	}
 }
