635c544236894675425d074f1877b7d23bad5726
Dolychuck <Dolychuck@hotmail.com>
2017-10-06 00:48:59 -0700

    Add first Kanji

---
+++ SamuraiKanjiPirate/Assets/Scripts/Kanji.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kanji : MonoBehaviour
+{
+	public float moveSpeed = 5f;	
+
+	void FixedUpdate ()
+	{
+		GetComponent<Rigidbody2D>().AddForce(Vector3.down * moveSpeed);
+	}
+}
---
+++ SamuraiKanjiPirate/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnTime1 = 2f;
+	public float spawnDelay1 = 3f;
+	public float spawnTime2 = 1f;
+	public float spawnDelay2 = 2f;	
+	public GameObject[] kanjis;		
+
+	void Start ()
+	{
+		InvokeRepeating("Spawn", spawnDelay1, spawnTime1);
+		InvokeRepeating("Spawn", spawnDelay2, spawnTime2);
+	}
+
+
+	void Spawn ()
+	{
+		int kanjisIndex = Random.Range(0, kanjis.Length);
+		Instantiate(kanjis[kanjisIndex],new Vector3(Random.Range(-7,7),7,0),transform.rotation);
+	}
+}
